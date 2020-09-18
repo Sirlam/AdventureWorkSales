e9794482faf6a663c5b06d2ae834445747646a00
Salam Olanrewaju <salam_lanre@yahoo.com>
2020-09-18 02:43:01 +0100

    CRUD for Product categories done

---
+++ AdventureWorksSales.Web/Controllers/ProductCategoryController.cs
@@ -0,0 +1,98 @@
+﻿using AdventureWorksSales.Core.DAC;
+using AdventureWorksSales.Core.Entities;
+using AdventureWorksSales.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdventureWorksSales.Web.Controllers
+{
+    public class ProductCategoryController : Controller
+    {
+        ProductCategoryDAC _productCategoryDAC;
+
+        public ProductCategoryController()
+        {
+            _productCategoryDAC = new ProductCategoryDAC();
+        }
+
+        // GET: ProductCategory
+        public ActionResult Index()
+        {
+            var model = new List<ProductCategoryModel>();
+
+            var items = _productCategoryDAC.SelectProductCategories();
+
+            foreach (var i in items)
+            {
+                ProductCategoryModel product = new ProductCategoryModel();
+
+                product.ProductCategoryID = i.ProductCategoryID;
+                product.Name = i.Name;
+                product.rowguid = i.rowguid;
+                product.ModifiedDate = i.ModifiedDate;
+
+                model.Add(product);
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new ProductCategoryModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategoryModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                PRODUCTCATEGORY category = new PRODUCTCATEGORY 
+                { 
+                    Name = model.Name,
+                    rowguid = Guid.NewGuid(),
+                    ModifiedDate = DateTime.Now,
+                };
+
+
+                _productCategoryDAC.InsertProductCategory(category);
+                ViewBag.Message = "Category created successfully";
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _productCategoryDAC.SelectCategoryById(id);
+            var model = new ProductCategoryModel();
+
+            model.ProductCategoryID = data.ProductCategoryID;
+            model.rowguid = data.rowguid;
+            model.Name = data.Name;
+            model.ModifiedDate = data.ModifiedDate;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductCategoryModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                PRODUCTCATEGORY category = _productCategoryDAC.SelectCategoryById(model.ProductCategoryID);
+
+                category.Name = model.Name;
+                category.ModifiedDate = DateTime.Now;
+                _productCategoryDAC.UpdateProductCategory(category);
+                ViewBag.Message = "Category updated successfully";
+            }
+            return View(model);
+        }
+    }
+}
--- AdventureWorksSales.Web/Models/ProductCategoryModel.cs
+++ AdventureWorksSales.Web/Models/ProductCategoryModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,8 @@ namespace AdventureWorksSales.Web.Models
     public class ProductCategoryModel
     {
         public int ProductCategoryID { get; set; }
+        [Required(ErrorMessage = "Product Category Name is required", AllowEmptyStrings = false)]
+        [DisplayName("Product Category Name")]
         public string Name { get; set; }
         public Guid rowguid { get; set; }
         public DateTime ModifiedDate { get; set; }
